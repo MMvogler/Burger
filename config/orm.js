1967b0e2ad822d90238b20572f8ae13ff0954096
Megan Vogler <mmphilmon@gmail.com>
2019-10-28 10:08:59 -0500

    initial commit

---
+++ config/orm.js
@@ -0,0 +1,13 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+var orm = {};
+
+selectAll()
+
+insertOne()
+
+updateOne()
+
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+-- In this file, write insert queries to populate the burgers table with at least three entries.
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double with Cheese Burger', true);
+
